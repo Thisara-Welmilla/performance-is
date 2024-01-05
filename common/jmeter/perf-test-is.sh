1bd5f2cc18883aaf3a589dd81928b94cf866d737
Madhavi Gayathri <47152272+mpmadhavig@users.noreply.github.com>
2024-01-05 14:00:15 +0530

    Set default concurrent users.

--- common/jmeter/perf-test-is.sh
+++ common/jmeter/perf-test-is.sh
@@ -222,8 +222,8 @@ elif [ "$concurrency" == "500-3000" ]; then
     echo "Running tests for concurrency level 500-3000"
     default_concurrent_users="500 1000 1500 2000 2500 3000"
 else
-    echo "Running tests for concurrency level 50-3000"
-    default_concurrent_users="50 100 150 300 500 1000 1500 2000 2500 3000"
+    echo "Running tests for concurrency level 50-500"
+    default_concurrent_users="50 100 150 300 500"
 fi
 
 if [[ -z $test_duration ]]; then
